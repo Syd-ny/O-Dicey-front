44b3e53abcfc9b8e815175e5d9eaf64baaafc4f8
Bruno Meckler <28674205+bruno-meckler@users.noreply.github.com>
2023-05-05 10:52:10 +0200

    Initial React component structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header-title">O'Dicey</h1>
+    </header>
+  );
+};
+
+export default Header;
