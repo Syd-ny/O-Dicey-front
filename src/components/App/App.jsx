f1281bcde7ba49e1e3f3b706fb3fcc1550f1609a
Baptiste-Loudin <loudinbaptiste@gmail.com>
2023-05-05 14:04:43 +0200

    Home page, footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import Home from "../Home/Home";
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <Footer />
     </>
   );
