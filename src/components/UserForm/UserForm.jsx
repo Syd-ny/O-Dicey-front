5cb74859b8bacf855a0e7c5659eb79070e68d388
Bruno Meckler <28674205+bruno-meckler@users.noreply.github.com>
2023-05-10 11:24:49 +0200

    Basic redux setup, should be able to handle user authentication, components modified to handle logged in state

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,10 +1,28 @@
 import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './UserForm.scss';
 import edit from '../../assets/edit-icon.svg';
 import Frame from '../Frame/Frame';
+import { actionSubmitLoginForm, actionUpdateFormField } from '../../actions/user';
 
 const UserForm = ({ formType }) => {
+  const dispatch = useDispatch();
+  const email = useSelector((state) => state.user.email);
+  const password = useSelector((state) => state.user.password);
+
+  const changeField = (name, value) => {
+    dispatch(actionUpdateFormField(name, value));
+  };
+
+  const handleChangeField = (event) => {
+    return changeField(event.target.id, event.target.value);
+  }
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    dispatch(actionSubmitLoginForm());
+  };
 
   if (formType === 'subscribe') {
     return (
@@ -35,13 +53,13 @@ const UserForm = ({ formType }) => {
   else if (formType === 'login') {
     return (
       <Frame title="Login">
-        <form className="userform-form">
+        <form className="userform-form" onSubmit={handleLogin}>
 
           <label htmlFor="email">E-mail :</label>
-          <input type="email" id="email" placeholder="Adresse e-mail" />
+          <input type="email" id="email" placeholder="Adresse e-mail" value={email} onChange={handleChangeField} />
 
           <label htmlFor="password">Mot de passe :</label>
-          <input type="password" id="password" placeholder="Mot de passe" />
+          <input type="password" id="password" placeholder="Mot de passe" value={password} onChange={handleChangeField} />
 
           <button type="submit">Envoyer</button>
         </form>
