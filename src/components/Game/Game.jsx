2ba67e42d669610e4544224cecd3eaa46b0a03b3
Bruno Meckler <28674205+bruno-meckler@users.noreply.github.com>
2023-05-15 16:54:26 +0200

    Began to implement other players avatars'

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -3,12 +3,14 @@ import GameMap from "./GameMap/GameMap";
 import GameTabs from "./GameTabs/GameTabs";
 
 import './Game.scss';
+import PlayerAvatars from "./PlayerAvatars/PlayerAvatars";
 
 const Game = () => {
   return (
     <div className="game">
       <GameHeader />
       <div className="game-main">
+        <PlayerAvatars />
         <GameMap />
         <GameTabs />
       </div>
---
+++ src/components/Game/PlayerAvatars/PlayerAvatars.jsx
@@ -0,0 +1,16 @@
+import './PlayerAvatars.scss';
+
+const PlayerAvatars = () => {
+  return (
+    <div className="game-avatars">
+      <ul className="game-avatars-list">
+        <li className="game-avatars-list-item">A</li>
+        <li className="game-avatars-list-item">B</li>
+        <li className="game-avatars-list-item">C</li>
+        <li className="game-avatars-list-item">D</li>
+      </ul>
+    </div>
+  );
+};
+
+export default PlayerAvatars;
